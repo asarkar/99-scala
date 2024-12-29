97de6b92fedcac6c5e73fa12ce5e08adf55a59c6
Abhijit Sarkar <contact@asarkar.com>
2024-12-28 18:03:27 -0800

    Upgrade scalafmt in order to get SIP-64 syntax support

--- bintree/test/src/package.scala
+++ bintree/test/src/package.scala
@@ -5,9 +5,8 @@ import munit.Assertions.{assert, assertEquals}
 given [A] => Conversion[Tuple, Array[Option[A]]] = nums =>
   nums.toArray.map:
     case None => None
-    case x => Some(x.asInstanceOf[A])
+    case x    => Some(x.asInstanceOf[A])
 
 def assertSameTrees[A](trees: List[Tree[A]], expected: List[Array[Option[A]]]): Unit =
   assertEquals(trees.size, expected.size)
   assert(trees.forall(expected.map(Tree.fromArray).contains))
-            
